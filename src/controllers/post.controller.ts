d7a7153aad9bc8f1527579fe9c95d7cf7078b571
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2025-03-19 11:32:42 +0500

    Update post.controller.ts

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,11 +1,28 @@
+
 import { Request, Response } from "express";
 import { handleError } from "../helpers/error.handler";
+import postModel, {IPost} from "../models/post.model";
 
-
-export default async function getPosts(request:Request,response:Response){
+export async function getPosts(request: Request, response: Response): Promise<void> {
   try {
-    
-  } catch (error:any) {
-    return handleError(response, 500, error.message);
+    const posts:IPost[] = await postModel.find();
+    response.status(200).json({ success: true, data: posts }); 
+  } catch (error: any) {
+    handleError(response, 500, error.message); 
   }
 }
+
+export async function createPost(request:Request,response:Response):Promise<void>{
+  try{
+    const {
+      title,
+      content,
+    } = request.body;
+
+    const post:IPost = await postModel.create({title,content});
+    response.status(201).json({success:true,data:post});
+  }catch(error:any){
+    handleError(response,500,error.message);
+  }
+}
+
